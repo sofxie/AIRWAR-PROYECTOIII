0723ee92b0fa813b15a731afb8268f1a378daad5
Proyecto II <sofia.xie.x@gmail.com>
2024-10-28 14:13:59 -0600

    Fondo y Player

--- AIRWAR - PROYECTO III/MainWindow.xaml.cs
+++ AIRWAR - PROYECTO III/MainWindow.xaml.cs
@@ -8,17 +8,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace AIRWAR___PROYECTO_III
 {
-    /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        int speed = 7; // Velocidad del Movimiento del Jugador
+
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            
+            timer.Tick += GameTimerEvent; // Timer del Juego
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Start();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e) 
+        {
+            Canvas.SetLeft(Player, Canvas.GetLeft(Player) - speed);
+            if (Canvas.GetLeft(Player) < 5 || Canvas.GetLeft(Player) + (Player.Width * 2) > Application.Current.MainWindow.Width)
+            {
+                speed = -speed;
+            }
         }
     }
 }
