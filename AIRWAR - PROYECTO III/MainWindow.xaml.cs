17aa454932bdeda4e8b43945e7031ac279ea2ab3
Asrai04 <a.saborio.1@estudiantec.cr>
2024-11-23 18:04:23 -0600

    Solve

--- AIRWAR - PROYECTO III/MainWindow.xaml.cs
+++ AIRWAR - PROYECTO III/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace AIRWAR___PROYECTO_III
             MyCanvas.Focus();
 
             ImageBrush playerImage = new ImageBrush(); // Dibuja la imagen del jugador
-            playerImage.ImageSource = new BitmapImage(new Uri("C:\\Users\\sofia\\source\\repos\\AIRWAR - PROYECTO III\\AIRWAR - PROYECTO III\\Imagen\\AntiAirCratf.png"));// Hay que cambiar la ruta
+            playerImage.ImageSource = new BitmapImage(new Uri("C:\\Users\\ariel\\Source\\AIRWAR-PROYECTOIII\\AIRWAR - PROYECTO III\\Imagen\\AntiAirCratf.png"));// Hay que cambiar la ruta
             Player.Fill = playerImage;
         }
         private void Window_KeyDown(object sender, KeyEventArgs e)
